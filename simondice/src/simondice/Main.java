42bef5dbbf4d48aacc20945d0fb36af69d0006a3
DavEsp2406 <david.espeton.aparacio@colegiolitterator.com>
2024-01-22 00:13:11 +0100

    Creación de las clases y los metodos

---
+++ simondice/src/simondice/Main.java
@@ -0,0 +1,15 @@
+package simondice;
+/**
+ * Este es mi proyecto de la segunda evalucion , Simon Dice 
+ * @author David Espeton Aparicio
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		Engine eng = new Engine();
+        
+        eng.start();
+        
+	}
+}
